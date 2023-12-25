a7dd62b5fb4817808c6b0337682971d352013c2c
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-12-25 16:02:17 +0100

    Basic implementation of ServiceWorker added

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,27 @@
-console.log('hello from the service worker');
+const assets = [
+    '/',
+    'app.js',
+    'styles.css',
+    'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700&display=swap',
+    'https://kit.fontawesome.com/376c1d5471.js'
+];
+
+const cacheKey = 'assets';
+
+self.addEventListener('install', async (event) => {
+    const cachePromise = new Promise(async (resolve) => {
+        const cache = await caches.open(cacheKey);
+        await cache.addAll(assets);
+        resolve();
+    })
+    event.waitUntil(cachePromise);
+});
+
+self.addEventListener('fetch', async (event) => {
+    const response = new Promise(async (resolve) => {
+        const cache = await caches.open(cacheKey);
+        const response = await cache.match(event.request);
+        resolve(response || fetch(event.request));
+    });
+    event.respondWith(response);
+});
