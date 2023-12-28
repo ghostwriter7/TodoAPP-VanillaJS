072890af9af5f946d78bfaec664de7b99a0301a3
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-12-28 21:19:02 +0100

    Firebase emulators initialized

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -1,11 +1,17 @@
 import { firebaseConfig } from "../consts/firebase-config.js";
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 
 export function initialize() {
     const firebase = initializeApp(firebaseConfig);
     const firestore = getFirestore(firebase);
     const auth = getAuth(firebase);
+
+    if (location.hostname === 'localhost') {
+        connectAuthEmulator(auth, 'http://localhost:9099');
+        connectFirestoreEmulator(firebase, 'localhost', 8080);
+    }
+
     return { firebase, firestore, auth };
 }
