16e666df6112333967569fbdf822a0c2446549a6
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-01-01 20:51:32 +0100

    Folder structure changed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     build: {
         outDir: './dist',
         target: 'esnext',
         sourcemap: true,
-        copyPublicDir: true
+        copyPublicDir: true,
     }
 });
